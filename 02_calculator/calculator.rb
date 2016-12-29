7948b2e0db4cec54eb96321f873904c6c04047a3
Mr_Matt <mmarkotany@gmail.com>
2016-12-29 16:57:22 +1100

    both spec and calc functionality updatedto replicate basic calulator functionality all passing spec tests

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,45 @@
 #write your code here
+def add(num1, num)
+sum = num1 + num
+return sum
+end
+
+def subtract(num, num1)
+sub = num - num1
+return sub
+end
+
+def sum(numbers)
+sum =0
+
+numbers.each do |n|
+sum += n
+end
+
+return sum
+end
+
+def multiply(*numbers)
+
+total = 1
+
+numbers.each do |x|
+total *= x
+end
+
+return total
+end
+
+def power(num,exp)
+func = num**exp
+return func
+end
+
+def factorial x
+  if x <= 1
+    1
+  else
+    x * factorial(x-1)
+  end
+end
+
