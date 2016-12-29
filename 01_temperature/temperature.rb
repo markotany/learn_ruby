cf5730fd67825cc3fabf9d9f27bf0cbbfbd631c3
Mr_Matt <mmarkotany@gmail.com>
2016-12-29 14:09:25 +1100

    code amended and passes all tests

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,17 @@
 #write your code here
+def ftoc(num)
+temp = num -32.0;
+temp *= 5.0
+temp /=9.0
+return temp
+end
+
+def ctof(num)
+temp = num*9.0
+temp /= 5.0
+temp += 32.0
+return temp
+end
+
+
+
