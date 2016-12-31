17852709d3643e137fe7ad1dcc8bbbba52162020
Mr_Matt <mmarkotany@gmail.com>
2016-12-31 12:48:49 +1100

    full functionality achieved as determined by spec

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,39 @@
 #write your code here
+#check multiple words
+#check multiple first letters
+
+def check(s)
+
+	return true if s[0]== ("a" || "e" || "i" || "o" || "u") 
+			
+	
+
+end
+
+
+def translate(s)
+#words = s.split(" ")
+ # words = words.map do |word|
+#letters = word.split(//)
+	words = s.split(" ")
+   words = words.map do |word|
+	letters = word.split('')
+	until letters[0] =~ /[aeiou]/ do
+	
+		if (letters[0] == "q" and letters[1]=="u")
+		letters << letters.shift(2)
+		else
+		letters << letters.shift
+		end
+
+	end
+	 letters << "ay"
+	letters.join
+   end
+   words.join(' ')
+
+end
+
+ 
+
+
