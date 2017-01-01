b16db36ef05b48ce9a05a0b0c3f8fb685f6d2e8b
Mr_Matt <mmarkotany@gmail.com>
2017-01-01 17:17:12 +1100

    file passed spec

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,16 @@
 class Book
-# write your code here
+ attr_reader :title
+
+	 def title=(string)
+	words = string.split(" ")
+	words[0] = words[0].capitalize
+
+		
+	words.map! { |word| 
+	["the","a","an","and","in","the","of"].include?(word) ? word : word.capitalize 
+		   }
+	 @title = words.join(" ")
+	end
 end
+
+
