4021d2714122c14ced9f573aca5ae3eb25c83d6b
Mr_Matt <mmarkotany@gmail.com>
2017-01-01 21:50:20 +1100

    :code passes specs

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,16 @@
 class Timer
   #write your code here
+attr_accessor :seconds
+
+	def initialize()
+		@seconds = 0
+	end
+
+	def time_string()
+		hours = @seconds/3600
+		minutes = @seconds/60 %60
+		seconds = @seconds%60
+
+		sprintf("%02d:%02d:%02d", hours, minutes, seconds)
+	end
 end
